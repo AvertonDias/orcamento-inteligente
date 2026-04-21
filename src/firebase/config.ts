becc1842e23235174d0371ae8a4c4951c3099a1b
Ton Aplicativos <tonaplicativos@gmail.com>
2026-04-21 15:46:42 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,8 @@
-'use client';
-
-/**
- * Configuração do Firebase.
- * 
- * IMPORTANTE: Exportamos o objeto firebaseConfig para que possa ser usado pela função de inicialização.
- */
 export const firebaseConfig = {
-  apiKey: "AIzaSyB0itwIN5vN7cJ73024xNbUwdKDPAjwiYw",
-  authDomain: "studio-319377216-183d5.firebaseapp.com",
-  projectId: "studio-319377216-183d5",
-  storageBucket: "studio-319377216-183d5.firebasestorage.app",
-  messagingSenderId: "29375793960",
-  appId: "1:29375793960:web:9440e42e4e4f5fb4009e1d"
+  "projectId": "studio-319377216-183d5",
+  "appId": "1:29375793960:web:9440e42e4e4f5fb4009e1d",
+  "apiKey": "AIzaSyB0itwIN5vN7cJ73024xNbUwdKDPAjwiYw",
+  "authDomain": "studio-319377216-183d5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "29375793960"
 };
